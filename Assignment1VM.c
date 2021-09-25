7641e07c8ba70ad2ced2a7bd8724b96d72405569
Timothy Phillips <timothyphillips1999@gmail.com>
2021-09-25 14:13:36 -0400

    Implemented Function to 'Get Instructions'

--- Assignment1VM.c
+++ Assignment1VM.c
@@ -6,10 +6,39 @@
      Marcos Chacon
 */
 
-// Repo Test
+#include <stdio.h>
+#define PAS_SIZE 500
+
+// Functional Prototypes
+int getInstructions(char *input, int *pas);
 
 // Simple Placeholder Implementation
-int main(void)
+int main(int argc, char **argv)
 {
-  ;
+  int pas[PAS_SIZE];
+
+  // Read instructions from file
+  getInstructions(argv[1], pas);
+
+  // Test File IO
+  printf("%d\n" pas[0]);
+}
+
+
+int getInstructions(char *input, int *pas)
+{
+  FILE *fp;
+  int temp;
+  int i = -1;
+
+  if ((fp = fopen(input, "r")) == NULL)
+    return -1;
+
+  while (!feof (fp) && fscanf (fp, "%d", &temp) && i++ < PAS_SIZE )
+    pas[i] = temp;
+
+  fclose(fp);
+
+  // change to return instruction length
+  return 0;
 }
