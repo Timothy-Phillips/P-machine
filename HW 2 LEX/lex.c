b9d89718595e5967e114e070bbb94a78a61728d9
eirineo <60438888+eirineo@users.noreply.github.com>
2021-10-23 17:11:30 -0400

    fix 11

--- HW 2 LEX/lex.c
+++ HW 2 LEX/lex.c
@@ -254,7 +254,7 @@ lexeme *lexanalyzer(char *input)
 		else if(isalpha(token) != 0)
 		{
 			int identLength = 0;
-			char *ident = calloc(sizeof(char) * 12);
+			char *ident = calloc(12, sizeof(char));
 			ident[identLength] = token;
 			identLength++;
 			
